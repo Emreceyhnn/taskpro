e30dd7e7e4f15f75c8c3777886f82df1a649d286
DESKTOP-CA43GQ2\Emre <emreceyhnn@gmail.com>
2026-01-14 19:26:39 +0300

    t

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { getAccessToken } from "../lib/token";
+const BACKEND_ENDPOINT = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: BACKEND_ENDPOINT,
   withCredentials: true,
 });
 
