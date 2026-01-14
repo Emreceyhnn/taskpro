cda0fddd2d8df8de01294f785b34163dc926609e
DESKTOP-CA43GQ2\Emre <emreceyhnn@gmail.com>
2026-01-14 18:15:49 +0300

    t

--- src/lib/theme.d.ts
+++ src/lib/theme.d.ts
@@ -1,7 +1,54 @@
 import "@mui/material/styles";
 
 declare module "@mui/material/styles" {
+  /* ================= ICON ================= */
+  interface TypeIcon {
+    primary: string;
+    secondary: string;
+  }
+
+  /* ================= TEXT ================= */
   interface TypeText {
+    greenText: string;
     sideBarText: string;
+  }
+
+  /* =============== BACKGROUND ============== */
+  interface TypeBackground {
+    dashboardBg: string;
+    sidebar: string;
+    header: string;
+    hoverBg: string;
+  }
+
+  /* ============== BUTTONS ================= */
+  interface TypeButtonPrimary {
+    buttonBg: string;
+    buttonBgHover: string;
+    iconColor: string;
+    iconBgColor: string;
+    primaryText: string;
+  }
+
+  interface TypeButtonSecondary {
+    buttonBg: string;
+    buttonBgHover: string;
+    iconColor: string;
+    iconBg: string;
+    primaryText: string;
+  }
+
+  /* ================= LOGO ================= */
+  interface TypeLogo {
+    color1: string;
+    color2: string;
+    text: string;
+  }
+
+  /* ================= SCROLL =============== */
+  interface TypeScroll {
+    color: string;
+    hover: string;
+    background: string;
   }
 }
