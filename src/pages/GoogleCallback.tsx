56643f2036db96c84246c4cd2cab4eba7f663add
DESKTOP-CA43GQ2\Emre <emreceyhnn@gmail.com>
2026-01-14 17:12:31 +0300

    t

--- src/pages/GoogleCallback.tsx
+++ src/pages/GoogleCallback.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { confirmGoogleOAuth } from "../api/auth";
+import { Box, CircularProgress, Stack, useTheme } from "@mui/material";
+import landingHero from "../assets/authpage.png";
 
 export default function GoogleCallback() {
   useEffect(() => {
@@ -7,9 +9,41 @@ export default function GoogleCallback() {
     if (!code) return;
 
     confirmGoogleOAuth(code).then(() => {
-      window.location.href = "/playground";
+      window.location.href = "/dashboard";
     });
   }, []);
 
-  return <p>Google ile giriş yapılıyor...</p>;
+  const theme = useTheme();
+
+  return (
+    <Box sx={{ minWidth: "100vw", minHeight: "100vh" }} bgcolor={"#fff"}>
+      <Box
+        sx={{
+          background:
+            "linear-gradient(180deg, rgba(196, 196, 196, 0) 25%, #BEDBB0 92.19%)",
+          minWidth: "100vw",
+          minHeight: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Stack
+          direction={"column"}
+          alignItems={"center"}
+          maxWidth={473}
+          spacing={4}
+        >
+          <img src={landingHero} width={162} height={162} />
+          <Stack direction={"row"} alignItems={"center"} spacing={1}>
+            <CircularProgress
+              sx={{
+                color: theme.palette.buttonPrimary.iconColor,
+              }}
+            />
+          </Stack>
+        </Stack>
+      </Box>
+    </Box>
+  );
 }
